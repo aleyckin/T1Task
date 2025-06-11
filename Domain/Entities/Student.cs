4a2fab26f40d697bb5ee485cc92aec70609d6f96
Артём Алейкин <aleyckin@mail.ru>
2025-06-11 16:21:37 +0400

    Добавьте файлы проекта.

---
+++ Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Student : IId
+    {
+        public string FullName { get; set; } = string.Empty;
+        public Guid CourseId { get; set; }
+    }
+}
